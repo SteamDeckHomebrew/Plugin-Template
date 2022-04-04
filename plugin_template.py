61c7898068d88870eb25fc647cf17f8c59e4c289
tza <marios8543@gmail.com>
2022-04-04 18:30:38 +0300

    Update plugin_template.py
    Added hot_reload parameter and __main method

--- plugin_template.py
+++ plugin_template.py
@@ -4,16 +4,23 @@ class Plugin:
     # The name of the plugin author
     author = "Template Author"
 
+    # If the plugin should be reloaded from a call to /plugins/reload or a file change
+    hot_reload = False
+
     # The HTML that will be loaded when selecting the plugin in the list
     main_view_html = "<html><body><h2>Hello World</h2></body></html>"
+
     # The HTML that will be used to display a widget in the plugin main page
     tile_view_html = ""
 
-
     # A normal method. It can be called from JavaScript using call_plugin_function("method_1", argument1, argument2)
-    async def method_1(*args):
+    async def method_1(self, *args):
         pass
 
     # A normal method. It can be called from JavaScript using call_plugin_function("method_2", argument1, argument2)
-    async def method_2(*args):
+    async def method_2(self, *args):
+        pass
+
+    # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
+    async def __main(self):
         pass
